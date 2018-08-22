78209f264ebdff6bca57def30c0f2e89494a1410
thebear19 <hikari_no_kuma@hotmail.com>
2018-08-22 15:23:08 +0700

    product detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ShoppingDetailsComponent } from './shopping-details/shopping-details.component';
+
+const routes: Routes = [
+  { path: 'details', component: ShoppingDetailsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/shopping-details/shopping-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-details',
+  templateUrl: './shopping-details.component.html',
+  styleUrls: ['./shopping-details.component.css']
+})
+export class ShoppingDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
